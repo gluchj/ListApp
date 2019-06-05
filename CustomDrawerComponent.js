5d73bc928134e8213bb0386402e618dbf5d87046
gluchj <joel.gluch@gmail.com>
2019-06-04 23:30:35 -0400

    custom menu complete

--- CustomDrawerComponent.js
+++ CustomDrawerComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { NavigationActions } from 'react-navigation';
 import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+//<ImageBackground source={require('./assets/icon.png')} style={{ width: 280, height: 220}}></ImageBackground>
 
 export default class CustomDrawerComponent extends Component {
 
@@ -8,14 +10,46 @@ export default class CustomDrawerComponent extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<View style={{ width: '100%', height: 23, backgroundColor: 'orangered' }}/>
 				<View style={styles.header}>
-					<ImageBackground source={require('./assets/icon.png')} style={{ width: 280, height: 220}}>
-					</ImageBackground>
+					<Text style={styles.title}>
+						List
+						<Text style={{color:'slategray'}}>.App
+						</Text>
+					</Text>
 				</View>
-				<Text>Menu Item 1</Text>
-				<Text>Menu Item 2</Text>
-				<Text>Menu Item 3</Text>
+				
+				<View style={{ flexDirection: 'row', marginLeft: 20,}}>	
+					<Icon 
+						name="account-circle"
+						size={26}
+						color="darkgrey"
+					/>	
+					<Text style={styles.menuitem} onPress={ () => { this.props.navigation.navigate('Profile') }}>
+						Profile
+					</Text>
+				</View>
+				
+				<View style={{ flexDirection: 'row', marginLeft: 20, }}>	
+					<Icon 
+						name="info"
+						size={26}
+						color="darkgrey"
+					/>	
+					<Text style={styles.menuitem}>
+						About
+					</Text>
+				</View>
+				
+				<View style={{ flexDirection: 'row', marginLeft: 20, }}>	
+					<Icon 
+						name="power-settings-new"
+						size={26}
+						color="darkgrey"
+					/>	
+					<Text style={styles.menuitem}>
+						Logout
+					</Text>
+				</View>
 			</View>
 		)
 	}
@@ -26,7 +60,25 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	header: {
-		margin: 15,
-		height: 230,
+		paddingTop: 20,
+		marginBottom: 25,
+		height: 150,
+		width: '100%',
+		backgroundColor: 'orangered',
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
+	title: {
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 35,
+	},
+	menuitem: {
+		color: '#2a2a2a',
+		width: '90%',
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginBottom: 25,
+		marginLeft: 10,
+	}
 });
